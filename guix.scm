b13335f753c36c0b3c8119ac6b89654730bb1547
Pjotr Prins <pjotr.public01@thebird.nl>
2026-04-12 20:30:07 +0200

    Add verilator 4 package

--- guix.scm
+++ guix.scm
@@ -1,7 +1,7 @@
 ;; BSG Bladerunner RISC-V toolchain (stage1: GCC + binutils, no newlib)
 (use-modules
   ((guix licenses) #:prefix license:)
-  (guix packages) (guix gexp) (guix build-system gnu)
+  (guix packages) (guix gexp) (guix git-download) (guix build-system gnu)
   (gnu packages algebra) (gnu packages autotools) (gnu packages base)
   (gnu packages bison) (gnu packages compression) (gnu packages commencement)
   (gnu packages curl) (gnu packages flex) (gnu packages gcc)
@@ -99,4 +99,43 @@ binutils 2.32) for the HammerBlade manycore architecture.  This is a stage1
 build without newlib; suitable for bare-metal programs with -nostdlib.")
     (license license:gpl3+)))
 
-bsg-riscv-toolchain
+;;;
+;;; Verilator 4.228 -- BSG Bladerunner needs v4.x (v5.x API incompatible)
+;;;
+
+(define-public verilator-4
+  (package
+    (name "verilator")
+    (version "4.228")
+    (source
+     (origin
+       (method git-fetch)
+       (uri (git-reference
+             (url "https://github.com/verilator/verilator")
+             (commit (string-append "v" version))))
+       (file-name (git-file-name name version))
+       (sha256
+        (base32 "1ahxwldlyxd0kandxia5dinxg33v08zbjpwfib65s11gqxvim1jf"))))
+    (build-system gnu-build-system)
+    (native-inputs
+     (list autoconf automake bison flex gettext-minimal perl python which))
+    (inputs (list perl python))
+    (arguments
+     (list
+      #:tests? #f
+      #:phases
+      #~(modify-phases %standard-phases
+          (replace 'bootstrap
+            (lambda _ (invoke "autoconf")))
+          (add-after 'unpack 'adjust-source
+            (lambda _
+              (substitute* "bin/verilator"
+                (("/bin/echo") "echo")))))))
+    (home-page "https://www.veripool.org/verilator/")
+    (synopsis "Fast Verilog/SystemVerilog simulator (v4.x)")
+    (description "Verilator compiles synthesizable Verilog and SystemVerilog
+into C++ or SystemC code.  This is version 4.228, needed for BSG Bladerunner
+HammerBlade simulation.")
+    (license license:lgpl3+)))
+
+verilator-4
